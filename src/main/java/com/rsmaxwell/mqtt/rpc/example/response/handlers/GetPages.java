8057efbde11078ceea4d5191a7aa50d9509e39c2
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-13 18:19:07 +0100

    Initial version

---
+++ src/main/java/com/rsmaxwell/mqtt/rpc/example/response/handlers/GetPages.java
@@ -0,0 +1,19 @@
+package com.rsmaxwell.mqtt.rpc.example.response.handlers;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.rsmaxwell.mqtt.rpc.common.Result;
+import com.rsmaxwell.mqtt.rpc.response.RequestHandler;
+
+public class GetPages extends RequestHandler {
+
+	private static final Logger logger = LogManager.getLogger(GetPages.class);
+
+	public Result handleRequest(Map<String, Object> args) throws Exception {
+		logger.traceEntry();
+		return success("[ 'one', 'two', 'three' ]");
+	}
+}
---
+++ src/main/java/com/rsmaxwell/mqtt/rpc/example/response/handlers/Quit.java
@@ -0,0 +1,19 @@
+package com.rsmaxwell.mqtt.rpc.example.response.handlers;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.rsmaxwell.mqtt.rpc.common.Result;
+import com.rsmaxwell.mqtt.rpc.response.RequestHandler;
+
+public class Quit extends RequestHandler {
+
+	private static final Logger logger = LogManager.getLogger(Quit.class);
+
+	public Result handleRequest(Map<String, Object> args) throws Exception {
+		logger.traceEntry();
+		return quit();
+	}
+}
